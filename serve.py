e1d79029a8448ce2f4710c3927528febcd60f2ea
padlichan <lili.berenyi@gmail.com>
2025-07-10 12:22:39 +0100

    Change port to 80

--- serve.py
+++ serve.py
@@ -2,6 +2,6 @@ from wsgiref import simple_server
 from app import app
 
 if __name__ == "__main__":
-    with simple_server.make_server("127.0.0.1", 8000, app) as httpd:
-        print("Serving on http://127.0.0.1:8000")
+    with simple_server.make_server("127.0.0.1", 80, app) as httpd:
+        print("Serving on http://127.0.0.1:80")
         httpd.serve_forever()
